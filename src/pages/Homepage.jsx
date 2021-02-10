52738daee7ac6b839f1fed5eb817ebdf33f2873e
Pedro Lugon <pedromotta20@gmail.com>
2021-02-09 21:36:50 -0300

    fixing style

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,15 +3,18 @@ import { Link } from "react-router-dom";
 
 import "../style/homepage.css";
 
-import Banner from "../assets/BANNER.mp4";
 import Footer from "../components/Footer";
+
+import Banner from "../assets/BANNER.mp4";
+import Sign from "../assets/sign.png";
 
 function Homepage() {
   return (
     <>
       <div className="homepage">
+        <img className="signimg" src={Sign} />
         <div className="video">
-          <video src={Banner} width={"100%"} autoPlay playsInline loop muted />
+          <video src={Banner} width={"60%"} autoPlay playsInline loop muted />
         </div>
         <div className="sign">
           <Link className="link" to="/about">
