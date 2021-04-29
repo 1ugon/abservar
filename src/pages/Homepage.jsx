c98933c6a0da98da7817a8aa9de6bfa319f98064
Pedro Lugon <pedromotta20@gmail.com>
2021-04-29 04:38:25 -0300

    :lipstick: refactoring project

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,9 +9,6 @@ import { FaRegCommentDots, FaEnvelope } from "react-icons/fa";
 import ReactWhatsapp from "react-whatsapp";
 import { useScrollToTop } from "../hooks/scroll";
 
-import { arts } from "../data/arts";
-import { motions } from "../data/motions";
-
 function Homepage() {
   useScrollToTop();
 
@@ -34,67 +31,23 @@ function Homepage() {
       <span className="cursor" />
       <div className="homepage">
         <div className="nav">
-          <div className="button" title="whatsapp">
-            <ReactWhatsapp
-              number="+556195518261"
-              message="oi, abservar! 👁"
-              element="div"
-            >
-              <FaRegCommentDots size={50} color="black" />
-            </ReactWhatsapp>
+          <div className="button">
+            <h2>formação</h2>
           </div>
-          <div className="button" title="e-mail">
-            <a href="mailto:abservar@gmail.com">
-              <FaEnvelope size={50} color="black" />
-            </a>
+          <div className="button">
+            <h2>habilidades</h2>
           </div>
         </div>
         <div className="mainabout">
-          <video
-            src={AboutVideo}
-            width={"100%"}
-            autoPlay
-            playsInline
-            loop
-            muted
-            className="aboutvideo"
-          />
-          <div className="aboutcel">
-            <strong>
-              <p>experimental audio-visual research</p>
-              <p>based in brasília, brazil</p>
-              <br />
-              <p>started in the professional field in 2014 as video editor</p>
-              <p>
-                currently studying audio-visual at Instituto Federal de Brasília
-              </p>
-              <br />
-              <p>
-                main interest include cinematography, art direction, motion &
-                sound design
-              </p>
-            </strong>
+          <div className="construction">
+            <h1>Estamos em construção por favor tenha paciência</h1>
           </div>
         </div>
         <div className="motions">
-          <div className="grid">
-            {motions.map((motion) => (
-              <div key={motion.id}>
-                <img src={motion.video} width={"100%"} />
-              </div>
-            ))}
-          </div>
+          <div className="grid"></div>
         </div>
         <div className="arts">
-          <div className="fade">
-            <div className="grid">
-              {arts.map((art) => (
-                <Link to={`/art/${art.slang}`} key={art.id}>
-                  <img src={art.imageHome} alt={art.id} className={art.class} />
-                </Link>
-              ))}
-            </div>
-          </div>
+          <div className="fade"></div>
         </div>
         <Footer />
       </div>
